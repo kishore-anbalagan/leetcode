78a91b7092074bd7a54137b6b723e2e495ee4a11
KISHORE A <155794021+kishore-anbalagan@users.noreply.github.com>
2025-11-26 10:11:10 +0530

    Time: 25 ms (38.87%), Space: 12.3 MB (80.81%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -3,8 +3,11 @@ class Solution(object):
         a=0 
         b=1
         c=0
-        for i in range(n):
-           c=a
-           a=b
-           b=c+b
-        return a  
+        if n==1:
+            return 1
+        for i in range(1,n):
+
+            c=a+b
+            a=b
+            b=c
+        return c
