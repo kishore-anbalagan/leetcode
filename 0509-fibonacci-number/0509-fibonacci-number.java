c07886b49810b448ae93db22ccb3c3a1df798738
KISHORE A <155794021+kishore-anbalagan@users.noreply.github.com>
2025-12-16 15:46:15 +0530

    Time: 0 ms (100%), Space: 42.1 MB (32.53%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -3,16 +3,16 @@ class Solution {
         int a=0;
         int b=1;
         int c=0;
-        if(n==1)
+        if(n<2)
         {
-            return 1;
+            return n;
         }
-        for(int i=1;i<n;i++)
+        for(int i=0;i<n;i++)
         {
             c=a+b;
             a=b;
             b=c;
         }
-        return c;
+        return a;
     }
 }
