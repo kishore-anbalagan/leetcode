96fa95126353388e2e4c138c0ac0e4f6466f96d0
KISHORE A <155794021+kishore-anbalagan@users.noreply.github.com>
2025-12-16 15:36:04 +0530

    Time: 605 ms (32.21%), Space: 12.4 MB (80.32%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,7 +1,5 @@
 class Solution(object):
     def fib(self, n):
-        if n==0:
-            return 0
-        if n==1:
-            return 1
+        if n<2:
+            return n;
         return self.fib(n-1)+self.fib(n-2)
